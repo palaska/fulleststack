ae2ef866720602ddc5c7419beb17dfd1d131a19a
Secil Dogan Palaska <secildogann@gmail.com>
2025-05-17 13:47:28 +0100

    lint fixes

--- apps/mobile/components/ui/TabBarBackground.tsx
+++ apps/mobile/components/ui/TabBarBackground.tsx
@@ -1,6 +1,7 @@
 // This is a shim for web and Android where the tab bar is generally opaque.
 export default undefined;
 
+// eslint-disable-next-line react-hooks-extra/no-unnecessary-use-prefix
 export function useBottomTabOverflow() {
   return 0;
 }
